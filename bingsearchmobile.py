39d8567715cb0f7a01a5fa0169ea0b02627bbd2e
blakecschmidt <blakecschmidt@gmail.com>
2018-04-11 01:27:00 -0500

    added full paths for both files

--- bingsearchmobile.py
+++ bingsearchmobile.py
@@ -3,7 +3,7 @@ import time
 import os
 from random import randint
 
-file = open("words.txt", "r")
+file = open("/home/pi/Documents/Python/Bing-Search/words.txt", "r")
 words = []
 for line in file:
     words.append(line)
